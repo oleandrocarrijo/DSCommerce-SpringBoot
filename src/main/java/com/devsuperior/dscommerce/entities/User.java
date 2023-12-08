8719c5b4426668e8fae5bcda82633ad8ee919748
Leandro Carrijo <7leandrobatista@gmail.com>
2023-12-08 11:49:34 -0300

    feat: Database seeding

--- src/main/java/com/devsuperior/dscommerce/entities/User.java
+++ src/main/java/com/devsuperior/dscommerce/entities/User.java
@@ -17,7 +17,7 @@ public class User {
     @Column(unique = true)
     private String email;
     private String phone;
-    private LocalDate birthdate;
+    private LocalDate birthDate;
     private String password;
 
     @OneToMany(mappedBy = "client")
@@ -32,7 +32,7 @@ public class User {
         this.name = name;
         this.email = email;
         this.phone = phone;
-        this.birthdate = birthdate;
+        this.birthDate = birthdate;
         this.password = password;
     }
 
@@ -69,11 +69,11 @@ public class User {
     }
 
     public LocalDate getBirthdate() {
-        return birthdate;
+        return birthDate;
     }
 
     public void setBirthdate(LocalDate birthdate) {
-        this.birthdate = birthdate;
+        this.birthDate = birthdate;
     }
 
     public String getPassword() {
