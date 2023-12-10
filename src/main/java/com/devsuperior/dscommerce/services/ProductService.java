e228be37a4e2bf535feab6ce116c32f3e4c6b8e4
Leandro Carrijo <7leandrobatista@gmail.com>
2023-12-10 11:54:18 -0300

    feat: PUT product

--- src/main/java/com/devsuperior/dscommerce/services/ProductService.java
+++ src/main/java/com/devsuperior/dscommerce/services/ProductService.java
@@ -32,14 +32,22 @@ public class ProductService {
 
     @Transactional
     public ProductDTO insert(ProductDTO dto) {
-
         Product entity = new Product();
+        copyDtoToEntity(dto, entity);
+        return new ProductDTO(repository.save(entity));
+    }
 
+    @Transactional
+    public ProductDTO update(Long id, ProductDTO dto) {
+        Product entity = repository.getReferenceById(id);
+        copyDtoToEntity(dto, entity);
+        return new ProductDTO(repository.save(entity));
+    }
+
+    private void copyDtoToEntity(ProductDTO dto, Product entity) {
         entity.setName(dto.getName());
         entity.setDescription(dto.getDescription());
         entity.setPrice(dto.getPrice());
         entity.setImgUrl(dto.getImgUrl());
-
-        return new ProductDTO(repository.save(entity));
     }
 }
