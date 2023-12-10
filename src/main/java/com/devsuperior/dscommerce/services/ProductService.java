5303c7824f95167102cdac60bc7c3c714f639aa5
Leandro Carrijo <7leandrobatista@gmail.com>
2023-12-10 11:20:05 -0300

    feat: POST new product

--- src/main/java/com/devsuperior/dscommerce/services/ProductService.java
+++ src/main/java/com/devsuperior/dscommerce/services/ProductService.java
@@ -29,4 +29,17 @@ public class ProductService {
         Page<Product> result = repository.findAll(pageable);
         return result.map(x -> new ProductDTO(x));
     }
+
+    @Transactional
+    public ProductDTO insert(ProductDTO dto) {
+
+        Product entity = new Product();
+
+        entity.setName(dto.getName());
+        entity.setDescription(dto.getDescription());
+        entity.setPrice(dto.getPrice());
+        entity.setImgUrl(dto.getImgUrl());
+
+        return new ProductDTO(repository.save(entity));
+    }
 }
